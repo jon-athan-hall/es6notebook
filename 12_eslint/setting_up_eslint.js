27a6b851fa701447ceba296229f35f8cc2f9fcfd
Jonathan Hall <jonathan.edward.hall@gmail.com>
2018-05-16 15:56:18 -0400

    Module 12 - ES Lint

---
+++ 12_eslint/setting_up_eslint.js
@@ -0,0 +1,23 @@
+/**
+ * Run this command to install the package.
+ * npm install -g eslint
+ * And then run this command to check the file.
+ * eslint path/to/file.js
+ */
+
+/**
+ * Create an .eslintrc file to control what errors
+ * are tested. Put this file in a particular folder, or
+ * in the global system folder.
+ */
+
+/**
+ * This command comes from Airbnb's github page.
+ * npm install -g eslint-config-airbnb eslint eslint-plugin-import
+ * eslint-plugin-react eslint-plugin-jsx-a11y
+ */
+
+/**
+ * Hone your own eslintrc file as you build apps. Fix
+ * the rules as they make sense for you and your team.
+ */
