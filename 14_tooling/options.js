8427a1f4b9263776fabe4182f8b0c97217435a43
Jonathan Hall <jonathan.edward.hall@gmail.com>
2018-05-21 12:52:16 -0400

    Module 14 - ES6 Tooling

---
+++ 14_tooling/options.js
@@ -0,0 +1,18 @@
+/**
+ * Not all browsers support ES6 syntax. But using Babel and polyfills,
+ * most browsers can fix that problem.
+ */
+
+/**
+ * Babel compiles ES6 code down to its ES5 equivalent.
+ * Use `npm init` to create a package.json file.
+ * Then `npm install babel-cli` for the compiler.
+ * Then `npm install babel-preset-env` for what ES6 features you need to compile.
+ * Create a .babelrc file to create for settings, or add a "babel" object
+ * to the package.json file, then finally add what browsers you'd like to support.
+ */
+
+/**
+ * Also, use polyfills to cover for older browsers too. Find a
+ * module at Babel, or visit polyfill.io for usage.
+ */
